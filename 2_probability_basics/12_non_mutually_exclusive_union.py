1a50f82cdeae75faf11880e9faa94da8e1505a84
Thomas Nield <thomasnield@live.com>
2020-05-14 06:58:22 -0500

    Create 12_non_mutually_exclusive_union.py

---
+++ 2_probability_basics/12_non_mutually_exclusive_union.py
@@ -0,0 +1,7 @@
+prob_roll_1_1to4 = 4.0 / 6.0
+prob_roll_2_1to4 = 4.0 / 6.0
+
+prob_1to4_either = prob_roll_1_1to4 + prob_roll_2_1to4 -
+	(prob_roll_1_1to4 * prob_roll_2_1to4)
+	
+print(prob_1to4_either)
