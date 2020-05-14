f3c600aa37cf810eb54fe86d71c576570a97ac91
Thomas Nield <thomasnield@live.com>
2020-05-14 06:57:23 -0500

    Create 11_non_mutually_exclusive_union_permutations.py

---
+++ 2_probability_basics/11_non_mutually_exclusive_union_permutations.py
@@ -0,0 +1,23 @@
+
+# Declare possible outcomes for both dice
+first_roll_outcomes = [1, 2, 3, 4, 5, 6]
+second_roll_outcomes = [1, 2, 3, 4, 5, 6]
+
+# Combine each outcome between both dice
+all_combinations = [(c,d) for c in second_roll_outcomes for d in first_roll_outcomes]
+
+# Find outcomes where first die is 1-4:
+first_is_1_thru_4 = [t for t in all_combinations if t[0] in range(1,5)]
+
+# Find outcomes where second die is 1-4:
+second_is_1_thru_4 = [t for t in all_combinations if t[1] in range(1,5)]
+
+# Overlap between both die outcomes: 
+overlap = set(first_is_1_thru_4).intersection(set(second_is_1_thru_4))
+print("OVERLAP: {}".format(overlap))
+
+# Calculate broken probability
+broken_probability_either_1_thru_4 = \
+    float(len(first_is_1_thru_4) + len(second_is_1_thru_4)) / len(all_combinations)
+    
+print("BROKEN PROBABILITY OF EITHER DIE BEING 1-4: {}".format(broken_probability_either_1_thru_4))
